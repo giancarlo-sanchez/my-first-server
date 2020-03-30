230988e102e4dd8077fa0976050f944a71d6c127
jank_ <jank_329@hotmail.com>
2020-03-30 16:07:35 -0500

    setting up simple server

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
 const http = require('http');
 
-http.createServer(function(request,response){
-    response.writeHead(200,{'Content-Type': 'text/html'});
-    response.write('<h1>Hello, world!</h1>');
-    response.end();
-}).listen(8080, function(){
-    console.log("listening for requests on port 8080...")
+http.createServer((req,res)=>{
+
+    res.writeHead(200,{'Content-Type': 'text/html'});
+    res.write("hey wassup hola que mas");
+    res.end();
+    //req.writeHead(200,{'Content-Type': 'text/html'});
+   //req.write('<h1>Hello, world!</h1>');
+   //req.end();
+}).listen(3000, ()=>{
+    console.log("escuchando en el puerto numero 3000...");
 });
